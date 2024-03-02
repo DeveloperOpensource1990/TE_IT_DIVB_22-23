0a930aa21e07777f2cc737ab5c2446098c8125a3
Hitesh Sachdev <116787644+DeveloperOpensource1990@users.noreply.github.com>
2024-03-02 15:50:00 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // Number of Fibonacci numbers to generate
+        int first = 0;
+        int second = 1;
+        System.out.println("Fibonacci Series:");
+        System.out.print(first + " " + second);
+        for (int i = 2; i < n; i++) {
+            int next = first + second;
+            System.out.print(" " + next);
+            first = second;
+            second = next;
+        }
+    }
+}
